d70a253127aa4b5a7016e85e05dd84c5bd09a5b5
jin-zhan05 <jinzhan@westlake.edu.cn>
2025-02-25 17:04:49 +0800

    Update main.py

--- Problem2/main.py
+++ Problem2/main.py
@@ -1,22 +1,8 @@
 def calculate_polynomial(n: int, x: int, xi_list: list) -> int:
-    """
-    计算多项式展开式 (x - x₁)(x - x₂)...(x - xn) 的值
-
-    
-    参数:
-    n -- 整数，表示xi的数量
-
-    x -- 整数
-
-    xi_list -- 包含n个整数的列表
-
-    
-    返回:
-    展开式的计算结果
-
-    """
-
-        
+    result=1
+    for xi in xi_list:
+        result*=(x-xi）
+                 
     return result
 
 if __name__ == "__main__":
